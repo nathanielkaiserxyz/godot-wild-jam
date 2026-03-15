705d080f5109301b56e725f4e87c4fe0b02356e7
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2026-03-14 21:22:47 -0500

    message for sam

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # godot-wild-jam
+art heist
+
+- make painting
+- art museum rejects you
+- steal their stuff
+
+
+needed 
+main menu
+art_reject scene
+world = outside and inside museum
+floor = floor of museum
+elevator = choose floor
+you escaped = how much money, time, security guards killed
+
+ 
--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,1 +1,13 @@
 extends Node
+
+var levels = [
+	"res://scenes/level/art_reject.tscn",
+	"res://scenes/level/world.tscn",
+	"res://scenes/level/floor_two.tscn"
+]
+
+func load_level(index: int):
+	if index < levels.size():
+		get_tree().change_scene_to_file(levels[index])
+	else:
+		print("You beat the game!")
