16736f956510df641cfe249a80d6567e4fd0140e
ooga-chaka <smkingftb@gmail.com>
2026-03-18 22:39:46 -0400

    changed camera angle and speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 100.0
-@export var dash_speed: float = 300.0
+@export var speed: float = 75.0
+@export var dash_speed: float = 180.0
 @export var dash_duration: float = 0.2
 
 
