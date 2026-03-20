928008b5776b016b06d32ce8b56518dd4c2e4cdd
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2026-03-19 21:01:36 -0500

    chasing guards

--- scripts/guard.gd
+++ scripts/guard.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 
-var run_speed = 25
+var run_speed = 100
 var player = null
 
+@onready var nav_agent = $NavigationAgent2D
+
 func _physics_process(delta):
-	velocity = Vector2.ZERO
 	if player:
-		velocity = position.direction_to(player.position) * run_speed
-	move_and_slide()
+		nav_agent.target_position = player.global_position
+		
+		if nav_agent.is_navigation_finished():
+			return
+			
+		var next_point = nav_agent.get_next_path_position()
+		velocity = global_position.direction_to(next_point) * run_speed
+		move_and_slide()
 
 func _on_detect_radius_body_entered(body: Node2D) -> void:
-	player = body
-	print("check")
-	
+	if body.name == "Player":
+		player = body
+
 func _on_detect_radius_body_exited(body: Node2D) -> void:
-	player = null
-	print("uncheck")
+	if body.name == "Player":
+		player = null
